1d78dafd51cb68cf1ef390223a67230a02d2b9ee
cherkasovmv-5 <mcher191@mail.ru>
2025-12-02 14:52:35 +0300

    End

--- 12_02_2025/main.py
+++ 12_02_2025/main.py
@@ -5,4 +5,15 @@ set_option("display.max_columns", None)
 
 data = read_csv("files/winemag-data-130k-v2.csv")
 
-print(data.isna)
+new_data = data.drop(columns="region_2")
+new_data = new_data.dropna(subset="country")
+
+mean_price = new_data["price"].sum() / new_data["price"].count()
+
+new_data["price"] = new_data["price"].fillna(value=mean_price)
+
+# print(new_data.loc[10:20, ["country", "price"]])
+# print(new_data[new_data.price > 300].loc[345:350, ["country", "price"]])
+# print(new_data[(new_data.price > 300) & (new_data.country == "Italy")])
+
+print(new_data.groupby(by=["country", "price"]).count())
